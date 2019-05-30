f4b099a93780431fbfb409729c70e65a25ba3e92
lvze <lvze@tedu.cn>
2019-05-30 20:47:56 +0800

    一对一，多对多映射

--- Flask/FlaskDemo04/run01.py
+++ Flask/FlaskDemo04/run01.py
@@ -62,6 +62,19 @@ class Student(db.Model):
     sname = db.Column(db.String(30), nullable=False)
     sage = db.Column(db.Integer, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
+    # 增加关联属性和反向引用关系属性，来表示与Course之间的多对多的关系
+    # 对Course类和Student类的影响如下：
+    # 1.在Student类中会有一个属性叫courses，来表示对student的课程查询
+    # 2.在Course类中会有一个属性叫students，来表示对course的学员的查询
+    courses = db.relationship(
+        "Course",   # 指定多对多的关系中的另一个类
+        secondary="student_course",  # 指定第三张关联表名
+        lazy="dynamic",  # 要使用courses数据的延迟加载模式
+        backref=db.backref(
+            "students",  # 加入到Course类中的属性名，表示对Student的查询
+            lazy="dynamic"  # 指定在Course中查询student的时候的延迟加载
+        )
+    )
 
 
 class Teacher(db.Model):
@@ -70,15 +83,23 @@ class Teacher(db.Model):
     tage = db.Column(db.Integer, nullable=False)
     # 增加一个外键列course_id，引用自Course类
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    # 增加一个关联属性，表示要引用的wife信息
+    # 关联属性：在Teacher中要增加哪个属性用于表示对Wife的引用
+    # 反向引用关系属性：在Teacher中设置但要增加到Wife中，表示的是在Wife中要增加哪个属性表示对Teacher的引用
+    wife = db.relationship(
+        'Wife',
+        backref='teacher',
+        uselist=False
+    )
 
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cname = db.Column(db.String(30))
     # 增加对Teacher的关联属性和反向引用关系属性
-    course_teachers = db.relationship(
+    teachers = db.relationship(
         "Teacher",
-        backref="teacher_course",
+        backref="course",
         lazy="dynamic"
     )
 
@@ -88,6 +109,20 @@ class Wife(db.Model):
     wname = db.Column(db.String(30))
     # 增加birthday
     birthday = db.Column(db.Date)
+    # 增加一个属性teacher_id，表示对Teacher类的主键引用，并且增加唯一约束
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), unique=True)
+
+
+# 创建类StudentCourse，映射到表名：student_course
+# 表示Student与Course之间的多对多的第三张关联表
+class StudentCourse(db.Model):
+    __tablename__ = "student_course"
+    id = db.Column(db.Integer, primary_key=True)
+    # 外键student_id，引用自student表的主键id
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    # 外键course_id，引用自course表的主键id
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+
 # 删除所有表结构
 # db.drop_all()
 # 将所有的实体类生成对应的表结构，前提：表不存在的情况下才能生成
@@ -218,30 +253,37 @@ def limit_views():
 
 @app.route('/07-page')
 def page_view():
-    # 每页显示记录数量
+    # 变量-pageSize,表示每页显示的记录数量
     page_size = 2
-    # 当前页
-    current_page = int(request.args.get('current_page', "1"))
-    ost = (current_page-1)*page_size
-    # 查询第current_page页数据
+    # 变量-currentPage,表示当前想看的页数
+    current_page = int(request.args.get('current_page', 1))
+    # 查询第currentPage页的数据
+    # 跳过(currentPage-1)*pageSize条数据,再获取前pageSize条数据
+    ost = (current_page - 1) * page_size
     users = db.session.query(Users).offset(ost).limit(page_size).all()
 
-    # 通过page_size和总记录数计算尾页页码
+    # 通过pageSize和总记录数计算尾页页码
     total_count = db.session.query(Users).count()
-    last_page = math.ceil(total_count/page_size)
+    last_page = math.ceil(total_count / page_size)
 
     # 计算上一页页码
+    # 如果currentPage大于1的话那么上一页就是currentPage-1,否则上一页就是1
     prev_page = 1
     if current_page > 1:
         prev_page = current_page - 1
 
     # 计算下一页页码
-    next_page = last_page
+    # 如果currentPage小于lastPage的话那么下一页就是currentPage+1,否则下一页就是尾页
+    next_age = last_page
     if current_page < last_page:
         next_page = current_page + 1
+
     return render_template('07-page.html', params=locals())
 
 
+# wifes = Wife.query.all()
+    # for w in wifes:
+    #     p
 @app.route('/08-aggr')
 def aggregat_views():
     # 查询Users实体中所有人的平均年龄
@@ -309,7 +351,6 @@ def upuser():
         else:
             user.isActive = True
         db.session.add(user)
-        flash("修改成功")
         return redirect('/05-users')
 
 
@@ -329,8 +370,135 @@ def regtea():
     tea = Teacher()
     tea.tname = "吕小小泽"
     tea.tage = 28
-    tea.teacher_course = course  # 底层是将course.id给了tea.course_id属性
+    tea.course = course  # 底层是将course.id给了tea.course_id属性
     db.session.add(tea)
+    return "ok"
+
+
+@app.route('/13-otm', methods=['GET', 'POST'])
+def otm():
+    if request.method == "GET":
+        # 1.查询Course实体类中所有的数据
+        courses = Course.query.all()
+        # 2.渲染模板
+        return render_template('13-otm.html', courses=courses)
+    else:
+        # 1.接收前端传递过来的数据(tname,tage,cid)
+        tname = request.form['tname']
+        tage = request.form['tage']
+        course_id = request.form['cid']
+        # 2.创建Teacher对象并且为属性赋值
+        tea = Teacher()
+        tea.tname = tname
+        tea.tage = tage
+        tea.course_id = course_id
+        # 3.将tea保存进数据库
+        db.session.add(tea)
+        return "注册Teacher成功"
+
+
+@app.route('/14-otm-query')
+def otm_query():
+    # 通过course对象找到对应的teacher们
+    # 1.1查询id为2的course的信息
+    # cour = Course.query.filter_by(id=2).first()
+    # print("课程名：", cour)
+    # # 1.2获取当前course所对应的teacher们
+    # teachers = cour.teachers.all()
+    # for tea in teachers:
+    #     print("姓名：%s，年龄%s" % (tea.tname, tea.tage))
+
+    # 查找qtx所教的课程
+    tea = Teacher.query.filter_by(tname="qtx").first()
+    cour = tea.course
+    print(tea.tname, cour.cname)
+    return "ok"
+
+
+@app.route('/15-otm-exer')
+def otm_exer():
+    # 1.查询出所有的课程
+    courses = Course.query.all()
+    # 2.获取前端提交过来的cid参数值，如果没有当0处理
+    cid = int(request.args.get('cid', 0))
+    # 如果cid的值是0的话，则查询所有老师的信息
+    # 如果cid的值非0则按照cid值查找老师信息
+    if cid == 0:
+        teachers = Teacher.query.all()
+    else:
+        # 方案1：通过cid到Teacher实体中查询数据
+        # teachers = Teacher.query.filter_by(course_id=cid).all()
+        # 方案2：通过cid获取课程信息，在通过课程找到对应的老师们
+        course = Course.query.filter_by(id=cid).first()
+        teachers = course.teachers.all()
+    # 4.讲课程和老师渲染到模板中
+    return render_template('15-otm-exer.html', params=locals())
+
+
+@app.route('/16-oto')
+def oto_views():
+    # 方案1：通过wife对象中的teacher_id属性表示要引用的teacher的主键
+    # wife = Wife()
+    # wife.wname = '如花'
+    # wife.teacher_id = 1
+    # db.session.add(wife)
+    # 方案2：通过wife对象中的teacher属性表示要引用的teacher对象
+    # wife = Wife()
+    # wife.wname = '小泽夫人'
+    # teacher = Teacher.query.filter_by(tname='吕泽').first()
+    # wife.teacher = teacher
+    # db.session.add(wife)
+
+    # 3.查询每一名wife所对应的teacher的信息
+    wifes = Wife.query.all()
+    for w in wifes:
+        print("夫人名：%s,老师名：%s" % (w.wname, w.teacher.tname))
+    # 4.查询每一名teacher所对应的wife的信息
+    # teachers = Teacher.query.all()
+    # for tea in teachers:
+    #     print("老师名：%s" % tea.tname)
+    #     if tea.wife:
+    #         print("夫人姓名：%s" % tea.wife.wname)
+    #     print("**********")
+    return "注册wife成功"
+
+
+@app.route('/17-mtm')
+def mtm_views():
+    # 1.增加关联数据
+    # sc = StudentCourse()
+    # sc.student_id = 1
+    # sc.course_id = 1
+    # db.session.add(sc)
+
+    # 2.获取小明对象以及python高级对象，再将两个对象关联到一起
+    # stu = Student.query.filter_by(sname='小明').first()
+    # cour = Course.query.filter_by(cname='python高级').first()
+    # # 将cour加入到stu的课程列表中
+    # stu.courses.append(cour)
+
+    # 3.小马同学选择了python基础课程
+    # stu = Student.query.filter_by(sname='小马').first()
+    # cour = Course.query.filter_by(cname='python基础').first()
+    # cour.students.append(stu)
+
+    # 4.删除关联数据：通过关联属性反向引用关系属性.remove(实体对象) 实现删除
+
+    # 5.查询小明所选择的课程
+    # stu = Student.query.filter_by(sname='小明').first()
+    # print("学员名：", stu.sname)
+    # print("所选课程：")
+    # courses = stu.courses.all()
+    # for cour in courses:
+    #     print(" 课程名称：", cour.cname)
+    
+    # 6.查询选修python基础的学员们
+    cour = Course.query.filter_by(cname='python基础').first()
+    print("课程名：", cour.cname)
+    print("学员们：")
+    students = cour.students.all()
+    for stu in students:
+        print(" 学员名：", stu.sname)
     return "ok"
 
 
---
+++ Flask/FlaskDemo04/templates/15.js
@@ -0,0 +1,3 @@
+/**
+ * Created by tarena on 19-5-30.
+ */
